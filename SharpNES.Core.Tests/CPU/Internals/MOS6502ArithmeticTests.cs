85919cc56668ee70536ca45f9a1b608fa92b07d9
German Valencia <machinarius92@gmail.com>
2021-01-02 21:42:36 -0500

    Moved the ASL test into the arithmetic test suite

--- SharpNES.Core.Tests/CPU/Internals/MOS6502ArithmeticTests.cs
+++ SharpNES.Core.Tests/CPU/Internals/MOS6502ArithmeticTests.cs
@@ -195,6 +195,121 @@ namespace SharpNES.Core.Tests.CPU.Internals {
     }
     #endregion
 
+    #region ASL
+    [Fact]
+    public void AslMustShiftTheALUInputRegisterLeftAndStoreItIntoTheAbsoluteMemoryAddress() {
+      ushort expectedAbsAddress = 0x1245;
+      _mockCpu.Setup(mock => mock.ReadALUInputRegister()).Returns(0x2);
+      _mockCpu.Setup(mock => mock.WriteToDataBus(expectedAbsAddress, 0x4)).Verifiable();
+      _mockCpu.SetupProperty(mock => mock.AbsoluteAddress, expectedAbsAddress);
+      _mockCpu.SetupProperty(mock => mock.StatusRegister, NESCpuFlags.Null);
+      _mockCpu.SetupGet(mock => mock.CurrentInstruction).Returns(
+        new CpuInstruction(
+          MOS6502Instruction.ASL, MOS6502AddressingMode.AbsoluteX, 
+          () => 1, () => 1, 1
+        )
+      );
+
+      _subject.ArithmeticShiftLeft();
+      _mockCpu.Verify();
+      Check.That(_mockCpu.Object.StatusRegister).Equals(NESCpuFlags.Null);
+    }
+
+    [Fact]
+    public void AslMustComputeTheCarryFlagProperly() {
+      ushort expectedAbsAddress = 0x1245;
+      _mockCpu.Setup(mock => mock.ReadALUInputRegister()).Returns(0xA0);
+      _mockCpu.Setup(mock => mock.WriteToDataBus(expectedAbsAddress, 0x40)).Verifiable();
+      _mockCpu.SetupProperty(mock => mock.AbsoluteAddress, expectedAbsAddress);
+      _mockCpu.SetupProperty(mock => mock.StatusRegister, NESCpuFlags.Null);
+      _mockCpu.SetupGet(mock => mock.CurrentInstruction).Returns(
+        new CpuInstruction(
+          MOS6502Instruction.ASL, MOS6502AddressingMode.AbsoluteX, 
+          () => 1, () => 1, 1
+        )
+      );
+
+      _subject.ArithmeticShiftLeft();
+      _mockCpu.Verify();
+      Check.That(_mockCpu.Object.StatusRegister).Equals(NESCpuFlags.CarryBit);
+    }
+
+    [Fact]
+    public void AslMustComputeTheNegativeFlagProperly() {
+      ushort expectedAbsAddress = 0x1245;
+      _mockCpu.Setup(mock => mock.ReadALUInputRegister()).Returns(0x7F);
+      _mockCpu.Setup(mock => mock.WriteToDataBus(expectedAbsAddress, 0xFE)).Verifiable();
+      _mockCpu.SetupProperty(mock => mock.AbsoluteAddress, expectedAbsAddress);
+      _mockCpu.SetupProperty(mock => mock.StatusRegister, NESCpuFlags.Null);
+      _mockCpu.SetupGet(mock => mock.CurrentInstruction).Returns(
+        new CpuInstruction(
+          MOS6502Instruction.ASL, MOS6502AddressingMode.AbsoluteX, 
+          () => 1, () => 1, 1
+        )
+      );
+
+      _subject.ArithmeticShiftLeft();
+      _mockCpu.Verify();
+      Check.That(_mockCpu.Object.StatusRegister).Equals(NESCpuFlags.Negative);
+    }
+
+    [Fact]
+    public void AslMustComputeTheZeroFlagProperly() {
+      ushort expectedAbsAddress = 0x1245;
+      _mockCpu.Setup(mock => mock.ReadALUInputRegister()).Returns(0x80);
+      _mockCpu.Setup(mock => mock.WriteToDataBus(expectedAbsAddress, 0x00)).Verifiable();
+      _mockCpu.SetupProperty(mock => mock.AbsoluteAddress, expectedAbsAddress);
+      _mockCpu.SetupProperty(mock => mock.StatusRegister, NESCpuFlags.Null);
+      _mockCpu.SetupGet(mock => mock.CurrentInstruction).Returns(
+        new CpuInstruction(
+          MOS6502Instruction.ASL, MOS6502AddressingMode.AbsoluteX, 
+          () => 1, () => 1, 1
+        )
+      );
+
+      _subject.ArithmeticShiftLeft();
+      _mockCpu.Verify();
+      Check.That(_mockCpu.Object.StatusRegister).Equals(NESCpuFlags.Zero | NESCpuFlags.CarryBit);
+    }
+
+    [Fact]
+    public void AslMustWriteTheResultToTheAccumulatorIfTheAddressingModeIsImplicit() {
+      ushort expectedAbsAddress = 0x1245;
+      _mockCpu.Setup(mock => mock.ReadALUInputRegister()).Returns(0x2);
+      _mockCpu.SetupProperty(mock => mock.AccumulatorRegister, (byte)0);
+      _mockCpu.SetupProperty(mock => mock.AbsoluteAddress, expectedAbsAddress);
+      _mockCpu.SetupProperty(mock => mock.StatusRegister, NESCpuFlags.Null);
+      _mockCpu.SetupGet(mock => mock.CurrentInstruction).Returns(
+        new CpuInstruction(
+          MOS6502Instruction.ASL, MOS6502AddressingMode.Implicit, 
+          () => 1, () => 1, 1
+        )
+      );
+
+      _subject.ArithmeticShiftLeft();
+
+      Check.That(_mockCpu.Object.StatusRegister).Equals(NESCpuFlags.Null);
+      Check.That(_mockCpu.Object.AccumulatorRegister).Equals(0x4);
+    }
+
+    [Fact]
+    public void AslMayNeverRequireMoreCycles() {
+      ushort expectedAbsAddress = 0x1245;
+      _mockCpu.Setup(mock => mock.ReadALUInputRegister()).Returns(0x2);
+      _mockCpu.Setup(mock => mock.WriteToDataBus(expectedAbsAddress, 0x4)).Verifiable();
+      _mockCpu.SetupProperty(mock => mock.AbsoluteAddress, expectedAbsAddress);
+      _mockCpu.SetupProperty(mock => mock.StatusRegister, NESCpuFlags.Null);
+      _mockCpu.SetupGet(mock => mock.CurrentInstruction).Returns(
+        new CpuInstruction(
+          MOS6502Instruction.ASL, MOS6502AddressingMode.AbsoluteX, 
+          () => 1, () => 1, 1
+        )
+      );
+
+      Check.That(_subject.ArithmeticShiftLeft()).IsEqualTo(0);
+    }
+    #endregion
+
     #region TestData
     #pragma warning disable CA1812
     // Taken from http://www.righto.com/2012/12/the-6502-overflow-flag-explained.html
